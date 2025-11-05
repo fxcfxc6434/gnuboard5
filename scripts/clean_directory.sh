e934fe8d06054e515ace5bfb511e2eb86fa6d131
fxcfxc6434 <fxcfxc@mz.co.kr>
2025-11-05 11:44:38 +0900

    Update clean_directory.sh

--- scripts/clean_directory.sh
+++ scripts/clean_directory.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# 그누보드가 설치될 디렉토리 경로
 DEPLOY_DIR="/var/www/html/gnuboard5"
+BACKUP_DIR="/tmp/gnuboard_backup_data"
 
-# 1. 만약 디렉토리가 이미 존재한다면, 통째로 삭제합니다.
-if [ -d "$DEPLOY_DIR" ]; then
-    rm -rf "$DEPLOY_DIR"
+# 1. 'data' 폴더가 있다면, /tmp로 잠시 백업합니다.
+if [ -d "$DEPLOY_DIR/data" ]; then
+    sudo mv "$DEPLOY_DIR/data" "$BACKUP_DIR"
 fi
 
-# 2. 배포를 위해 비어있는 새 디렉토리를 생성합니다.
-mkdir -p "$DEPLOY_DIR"
+# 2. 배포 폴더를 청소합니다.
+if [ -d "$DEPLOY_DIR" ]; then
+    sudo rm -rf "$DEPLOY_DIR"
+fi
+sudo mkdir -p "$DEPLOY_DIR"
 
-# 3. CodeDeploy가 파일 복사 후 권한 문제를 일으키지 않도록 미리 소유권을 변경합니다.
-chown apache:apache "$DEPLOY_DIR"
+# 3. 백업해둔 'data' 폴더를 다시 원위치시킵니다.
+if [ -d "$BACKUP_DIR" ]; then
+    sudo mv "$BACKUP_DIR" "$DEPLOY_DIR/data"
+fi
+
+# 4. 소유권 설정 (data 폴더 포함)
+sudo chown -R apache:apache "$DEPLOY_DIR"
